15ef714a7c2c18243b1f17fbc086123be2ffcc62
JakeMaciver <Jacobmaciverwork@gmail.com>
2023-05-02 09:41:36 +0100

    enable CORS on backend server

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { getItems } from './controllers/itemControllers';
+import cors from 'cors';
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get(`/api/items`, getItems);
