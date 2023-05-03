224c11859d85a4b96899396b7093d728f5576ce9
“margaritisarg” <margaritis1argyrou@gmail.com>
2023-05-03 15:34:15 +0100

    api and other bits done

---
+++ front-end/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const recycleLand = axios.create({
+    baseURL: 'https://recycland.onrender.com/api'
+})
+
+export const fetchAllItems = () => {
+    return recycleLand.get(`/items`).then((response) => {
+        return response.data.items;
+    })
+}
