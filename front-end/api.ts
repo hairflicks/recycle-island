fa0f6a38b1014ee2e6d94e3fe862b20794a924a3
JakeMaciver <Jacobmaciverwork@gmail.com>
2023-05-03 16:59:05 +0100

    add functionality to the sign up page, able to sign up and add a user to the database.

--- front-end/api.ts
+++ front-end/api.ts
@@ -9,3 +9,11 @@ export const fetchAllItems = () => {
         return response.data.items;
     })
 }
+
+export const postUser = async (name: String, username: String, password: String) => {
+  const userToPost = {
+    name, username, hash: password
+  }
+  const data = await recycleLand.post(`/users`, userToPost)
+  return data
+}
--- front-end/components/Login.tsx
+++ front-end/components/Login.tsx
@@ -1,90 +1,105 @@
-import { useState } from 'react';
-import { View, Text, StatusBar, StyleSheet, Button, TextInput, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, useColorScheme } from 'react-native';
 import { styles } from './StyleSheetCSS';
+import { postUser } from '../api';
+import { Color } from 'three';
+import { AxiosError } from 'axios';
 
-function Login({ navigation }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [slider, setSlider] = useState(true);
-  
-    const handleLogin = (event: object) => {
-      console.log(event)
-      console.log(`Logging in with username: ${username}, password: ${password}`);
-      setSlider(true)
-    };
+type LoginProps = {
+	navigation: { navigate: Function };
+};
 
-    const handleSignUp = () => {
-      setSlider(false)
+function Login({ navigation }: LoginProps) {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [name, setName] = useState('');
+	const [slider, setSlider] = useState(true);
+  const [error, setError] = useState('');
+
+	const handlePresentPage = (event: object) => {
+		console.log(`Logging in with username: ${username}, password: ${password}`);
+		setSlider((slider) => !slider);
+	};
+
+	const handleSignUp = async () => {   
+    try{
+      await postUser(name, username, password);
+      setSlider((slider) => !slider)
+      setPassword('')
+      setName('')
+    } catch (error: any){
+      setError(error.response.data.message);
     }
+	};
 
-    const handleNavigation = () => {
-      navigation.navigate('Island')
-    }
-  
-    return (
-      <View style={styles.container}>
-        <View style={styles.slider}>
-          
-          <TouchableOpacity style={styles.invertedButton} onPress={handleLogin}>
-            <Text style={styles.invertedButtonText}>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-            <Text style={styles.buttonText}>Sign Up</Text>
-          </TouchableOpacity>
-        </View>
+	const handleLogin = (): void => {
+		navigation.navigate('Island');
+	};
 
-        {slider
-        ? 
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Username"
-            value={username}
-            onChangeText={setUsername}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            secureTextEntry={true}
-            value={password}
-            onChangeText={setPassword}
-          />
-          <TouchableOpacity style={styles.button} onPress={handleNavigation}>
-            <Text style={styles.buttonText}>Login</Text>          
-          </TouchableOpacity>
+		return (
+			<View style={styles.container}>
+				<View style={styles.slider}>
+					<TouchableOpacity
+						style={styles.invertedButton}
+						onPress={handlePresentPage}
+					>
+						<Text style={styles.invertedButtonText}>Login</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={handlePresentPage}>
+						<Text style={styles.buttonText}>Sign Up</Text>
+					</TouchableOpacity>
+				</View>
 
+				{slider ? (
+					<View style={styles.inputContainer}>
+						<TextInput
+							style={styles.input}
+							placeholder='Username'
+							value={username}
+							onChangeText={setUsername}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Password'
+							secureTextEntry={true}
+							value={password}
+							onChangeText={setPassword}
+						/>
+						<TouchableOpacity style={styles.button} onPress={handleLogin}>
+							<Text style={styles.buttonText}>Login</Text>
+						</TouchableOpacity>
+					</View>
+				) : (
+					<View style={styles.inputContainer}>
+						<TextInput
+							style={styles.input}
+							placeholder='Name'
+							value={name}
+							onChangeText={setName}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Username'
+							value={username}
+							onChangeText={setUsername}
+						/>
+						<TextInput
+							style={styles.input}
+							placeholder='Password'
+							secureTextEntry={true}
+							value={password}
+							onChangeText={setPassword}
+						/>
+						<TouchableOpacity style={styles.button} onPress={handleSignUp}>
+							<Text style={styles.buttonText}>Create Account</Text>
+						</TouchableOpacity>
+					</View>
+				)}
+				{error ? (
+					<Text>{error}</Text>
+				) : null}
+			</View>
+		);
+}
 
-
-        </View>
-        : 
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Name"
-            value={username}
-            onChangeText={setUsername}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            secureTextEntry={true}
-            value={password}
-            onChangeText={setPassword}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Email"
-            value=''
-            onChangeText={setUsername}
-          />
-          <TouchableOpacity style={styles.button} onPress={handleNavigation}>
-            <Text style={styles.buttonText}>Create Account</Text>
-          </TouchableOpacity>
-        </View>
-        }
-      </View>
-    );
-  }
-  
-  
-  export default Login;
+export default Login;
