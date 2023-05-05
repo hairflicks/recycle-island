e80317526f0bb23e2834d0d890a81480a1c36338
Viccart <hannahcarter89@hotmail.com>
2023-05-05 17:02:20 +0100

    flippable card

--- front-end/components/FlippableCard.tsx
+++ front-end/components/FlippableCard.tsx
@@ -1,52 +1,52 @@
-import React, { useState } from 'react';
-import {
-	Text,
-	TouchableOpacity,
-	Animated,
-} from 'react-native';
-import { styles } from './StyleSheetCSS';
+import React, { useState } from "react";
+import { Text, TouchableOpacity, Animated, Image } from "react-native";
+import { styles } from "./StyleSheetCSS";
 
 const FlippableCard = ({ currentUser, model }) => {
-	const [isFlipped, setIsFlipped] = useState(false);
-	const rotateY = useState(new Animated.Value(0))[0];
+  const [isFlipped, setIsFlipped] = useState(false);
+  const rotateY = useState(new Animated.Value(0))[0];
 
-	const flipCard = () => {
-		setIsFlipped(!isFlipped);
-		Animated.timing(rotateY, {
-			toValue: isFlipped ? 0 : 1,
-			duration: 350,
-			useNativeDriver: true,
-		}).start();
-	};
+  const flipCard = () => {
+    setIsFlipped(!isFlipped);
+    Animated.timing(rotateY, {
+      toValue: isFlipped ? 0 : 1,
+      duration: 350,
+      useNativeDriver: true,
+    }).start();
+  };
 
-	const frontInterpolate = rotateY.interpolate({
-		inputRange: [0, 1],
-		outputRange: ['0deg', '180deg'],
-	});
+  const frontInterpolate = rotateY.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "180deg"],
+  });
 
-	const backInterpolate = rotateY.interpolate({
-		inputRange: [0, 1],
-		outputRange: ['180deg', '360deg'],
-	});
+  const backInterpolate = rotateY.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["180deg", "360deg"],
+  });
 
-	const frontAnimatedStyle = {
-		transform: [{ rotateY: frontInterpolate }],
-	};
+  const frontAnimatedStyle = {
+    transform: [{ rotateY: frontInterpolate }],
+  };
 
-	const backAnimatedStyle = {
-		transform: [{ rotateY: backInterpolate }],
-	};
+  const backAnimatedStyle = {
+    transform: [{ rotateY: backInterpolate }],
+  };
 
-	return (
-		<TouchableOpacity style={styles.cardContainer} onPress={flipCard}>
-			<Animated.View style={[styles.card, styles.front, frontAnimatedStyle]}>
-				<Text style={styles.text}>Hello</Text>
-			</Animated.View>
-			<Animated.View style={[styles.card, styles.back, backAnimatedStyle]}>
-				<Text style={styles.text}>Goodbye</Text>
-			</Animated.View>
-		</TouchableOpacity>
-	);
+  return (
+    <TouchableOpacity onPress={flipCard}>
+      <Animated.View style={[styles.card, styles.front, frontAnimatedStyle]}>
+        <Text style={styles.shopItem}>{model.itemDisplayName}</Text>
+        <Image
+          style={styles.shopItemImage}
+          source={require(`../assets/Chicken/Chicken.png`)}
+        />
+      </Animated.View>
+      <Animated.View style={[styles.card, styles.back, backAnimatedStyle]}>
+        <Text style={styles.text}>Goodbye</Text>
+      </Animated.View>
+    </TouchableOpacity>
+  );
 };
 
 export default FlippableCard;
